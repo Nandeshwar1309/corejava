83edd7f7f72cb19b9d7097cfa133276300b45746
DELL PC <DELL PC@DESKTOP-VKQRU13>
2024-07-03 15:41:53 +0530

    second commit

---
+++ corejavaprogram/src/com/tnsif/encapsulation/Encapsulationdemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.encapsulation;
+
+public class Encapsulationdemo {
+	int serialnumber;
+	String name;
+	int age;
+	
+	void show() {
+		System.out.println("Serialnumber:"+serialnumber+"name:"+name+"age:"+age);
+	}
+
+}

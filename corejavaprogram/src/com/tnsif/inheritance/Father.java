0a2a5f03b0ea157e43b7cf78c04a3e9d6c0355ff
DELL PC <DELL PC@DESKTOP-VKQRU13>
2024-07-08 21:33:40 +0530

    commit

---
+++ corejavaprogram/src/com/tnsif/inheritance/Father.java
@@ -0,0 +1,16 @@
+package com.tnsif.inheritance;
+
+public class Father {
+	
+	// properties
+	
+	int money=40000000;
+	String car="BMW";
+	
+	//method
+	
+	void drinking() {
+		System.out.println("Coffee");
+	}
+
+}
---
+++ corejavaprogram/src/com/tnsif/inheritance/Supermain.java
@@ -0,0 +1,15 @@
+package com.tnsif.inheritance;
+
+public class Supermain {
+	
+	public static void main(String[] args) {
+		
+		Student s=new Student("Ram",647287307,"hyderabad",987654323);
+		System.out.println(s);
+		
+		Student s1=new Student(5,"Sriindu");
+		System.out.println(s1);
+		
+	}
+
+}
